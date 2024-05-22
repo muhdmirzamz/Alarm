405a4d9f9775902f9f515a35bdd4c72c0aa3028f
muhdmirzamz <muhdmirzamz@gmail.com>
2024-05-22 15:35:24 +0800

    edited gitignore

---
+++ Alarm/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  Alarm
+//
+//  Created by Muhd Mirza on 22/5/24.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
