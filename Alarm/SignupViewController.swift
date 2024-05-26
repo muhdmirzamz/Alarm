a90bf0f536d376d478448136ab09bea375e2032b
muhdmirzamz <muhdmirzamz@gmail.com>
2024-05-26 14:27:09 +0800

    implemented sign up

--- Alarm/SignupViewController.swift
+++ Alarm/SignupViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import FirebaseAuth
+
 class SignupViewController: UIViewController {
     
     @IBOutlet var emailTextfield: UITextField!
@@ -19,7 +21,51 @@ class SignupViewController: UIViewController {
     }
     
     @IBAction func signup() {
+        guard let email = emailTextfield.text else {
+            return
+        }
         
+        guard let password = passwordTextfield.text else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            
+            if let _ = result {
+                let alert = UIAlertController.init(title: "Success", message: "Signed up successfully", preferredStyle: .alert)
+                
+                let okAction = UIAlertAction.init(title: "OK", style: .default) { action in
+                    DispatchQueue.main.async {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+                
+                alert.addAction(okAction)
+                
+                DispatchQueue.main.async {
+                    self.present(alert, animated: true)
+                }
+            }
+            
+            if let error = error {
+                let alert = UIAlertController.init(title: "Error", message: "Invalid email", preferredStyle: .alert)
+                let okAction = UIAlertAction.init(title: "OK", style: .default) { action in
+                    // reset all the fields if email input is invalid
+                    self.emailTextfield.text = ""
+                    self.passwordTextfield.text = ""
+                }
+                
+                alert.addAction(okAction)
+                
+                DispatchQueue.main.async {
+                    self.present(alert, animated: true)
+                }
+            }
+        }
+    }
+    
+    @IBAction func dismissViewController() {
+        self.dismiss(animated: true, completion: nil)
     }
     
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Alarm
 
-Basic alarm app for me to practise using Swift Package Manager, Firebase and local notifications on iOS
+Basic alarm app for me to practise using Swift Package Manager, Firebase and local notifications on iOS. This has a sign up, login mechanism just for fun and practice.
+
+### Note
+
+You do need your own firebase database for this
