8fdde018c8fb11e1d84fd1594a851d90bd6342bd
muhdmirzamz <muhdmirzamz@gmail.com>
2024-06-19 11:54:37 +0800

    added new alarm entity class

---
+++ Alarm/AddAlarmViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddAlarmViewController.swift
+//  Alarm
+//
+//  Created by Muhd Mirza on 6/6/24.
+//
+
+import UIKit
+
+class AddAlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Alarm/Alarm.swift
@@ -0,0 +1,15 @@
+//
+//  Alarm.swift
+//  Alarm
+//
+//  Created by Muhd Mirza on 19/6/24.
+//
+
+import Foundation
+
+class Alarm {
+    var key: String?
+    
+    var alarmName: String?
+    var timestamp: String?
+}
