be9885d85631d6ecf7eaa82956ad269dd6a734b7
muhdmirzamz <muhdmirzamz@gmail.com>
2024-06-29 09:24:03 +0800

    Added new utilities class

---
+++ Alarm/Utilities.swift
@@ -0,0 +1,19 @@
+//
+//  Utilities.swift
+//  Alarm
+//
+//  Created by Muhd Mirza on 29/6/24.
+//
+
+import Foundation
+
+class Utilities {
+    func getStringForDate(date dateObject: Date) -> String {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.sss'Z'"
+        
+        return dateFormatter.string(from: dateObject)
+    }
+}
